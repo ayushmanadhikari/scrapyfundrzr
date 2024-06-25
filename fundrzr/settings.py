f159392c2d8c9ddfc2126d1566f7429f376d246a
Ayushman Adhikari <unixparadoxfb@gmail.com>
2024-06-25 21:11:10 +0545

    rotating user agents middleware addded

--- fundrzr/settings.py
+++ fundrzr/settings.py
@@ -51,9 +51,10 @@ COOKIES_ENABLED = True
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    "fundrzr.middlewares.FundrzrDownloaderMiddleware": 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+    #"fundrzr.middlewares.FundrzrDownloaderMiddleware": 543,
+    'scrapy_user_agents.middlewares.RandomUserAgentMiddleware': 400,
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
